69e923d2847b079bb6de6456e3081e7929195138
Brock Tubre <brock.tubre@csra.com>
2018-04-11 19:52:09 -0500

    Hitting orbit API

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
+import {inject} from 'aurelia-framework';
+import { HttpClient, HttpResponseMessage } from 'aurelia-http-client';
+
+@inject (HttpClient)
 export class App {
-  message = 'Hello World!';
+
+  constructor(private httpClient: HttpClient){
+    this.getAllStudents();
+  }
+
+  getAllStudents(){
+    this.httpClient
+    .get('http://orbit.brocktubre.com/orbit-api/all-students')
+    .then((value: any) => {
+      console.log(value);
+    });
+  }
 }
---
+++ src/notes.ts
@@ -0,0 +1,7 @@
+export class Notes {    
+  message: string;
+  
+  constructor() {
+    this.message = 'Hello world';
+  }
+}
