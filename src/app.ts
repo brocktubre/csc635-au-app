4ba070fabfe9f5aca6e73b5d45fe469d52f229eb
Brock Tubre <brock.tubre@csra.com>
2018-04-18 16:47:48 -0500

    Progress

--- src/app.ts
+++ src/app.ts
@@ -9,22 +9,56 @@ export class App {
   public loadingNotes: boolean;
   public notes: Array<Notes>;
   public emptyNotes: string;
+  public submitted: boolean;
+  public submitError: string;
+
+  public noteTitle: string;
+  public noteNote: string;
 
   constructor(private httpClient: HttpClient){
     this.getAllStudents();
     this.year = new Date().getFullYear();
     this.loadingNotes = true;
-    this.emptyNotes = 'There are no notes in you list';
+    this.emptyNotes = 'There are no notes in your list.';
+    this.notes = new Array<Notes>();
+    this.submitted = false;
+    this.submitError = null;
   }
 
   getAllStudents(){
     this.httpClient
     .get('http://notesapplication.brocktubre.com/api/v1/notes')
     .then((value: any) => {
-      this.loadingNotes = false;
-      this.notes = value.response;
       debugger;
+      this.loadingNotes = false;
+      this.notes = JSON.parse(value.response);
       console.log(this.notes);
     });
+  }
+
+  addNewNote(){
+    console.log('Submit new note to API.');
+    this.submitted = true;
+    if (!this.noteNote || !this.noteTitle) {
+      this.submitError = 'Please enter in all fields.';
+      this.submitted = false;
+    }
+    else{
+      let note = new Notes();
+      note.note = this.noteNote;
+      note.title = this.noteTitle;
+      console.log('Sending note to API to save.' + note);
+      // this.httpClient
+      // .get('http://notesapplication.brocktubre.com/api/v1/notes')
+      // .then((value: any) => {
+      //   debugger;
+      //   this.loadingNotes = false;
+      //   this.notes = JSON.parse(value.response);
+      //   console.log(this.notes);
+      // });
+    }
+
+
+
   }
 }
--- src/notes.model.ts
+++ src/notes.model.ts
@@ -1,6 +1,12 @@
+import { Category } from './category.model';
+import { User } from 'user.model';
+
 export class Notes {
   public id: number;
   public isDeleted: boolean;
   public note: string;
   public title: string;
+  public createdOn: Date;
+  public user: User;
+  public category: Category;
 }
