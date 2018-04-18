e74fcbacb3057aa1adbdd603dedbf49d45829d29
Brock Tubre <brock.tubre@csra.com>
2018-04-18 08:49:31 -0500

    Work in progress

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
 import {inject} from 'aurelia-framework';
 import { HttpClient, HttpResponseMessage } from 'aurelia-http-client';
+import { Notes } from 'notes.model';
 
 @inject (HttpClient)
 export class App {
 
   public year: number;
   public loadingNotes: boolean;
-  public notes;
+  public notes: Array<Notes>;
+  public emptyNotes: string;
 
   constructor(private httpClient: HttpClient){
     this.getAllStudents();
     this.year = new Date().getFullYear();
     this.loadingNotes = true;
+    this.emptyNotes = 'There are no notes in you list';
   }
 
   getAllStudents(){
@@ -19,7 +22,9 @@ export class App {
     .get('http://notesapplication.brocktubre.com/api/v1/notes')
     .then((value: any) => {
       this.loadingNotes = false;
-      console.log(value);
+      this.notes = value.response;
+      debugger;
+      console.log(this.notes);
     });
   }
 }
---
+++ src/notes.model.ts
@@ -0,0 +1,6 @@
+export class Notes {
+  public id: number;
+  public isDeleted: boolean;
+  public note: string;
+  public title: string;
+}
