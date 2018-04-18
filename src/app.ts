1cd20127f0c80bb9ff54b2f12b584d76311e0996
Brock Tubre <brock.tubre@csra.com>
2018-04-17 20:46:31 -0500

    Work in progress

--- src/app.ts
+++ src/app.ts
@@ -4,14 +4,20 @@ import { HttpClient, HttpResponseMessage } from 'aurelia-http-client';
 @inject (HttpClient)
 export class App {
 
+  public year: number;
+  public loadingNotes: boolean;
+
   constructor(private httpClient: HttpClient){
     this.getAllStudents();
+    this.year = new Date().getFullYear();
+    this.loadingNotes = true;
   }
 
   getAllStudents(){
     this.httpClient
-    .get('http://orbit.brocktubre.com/orbit-api/all-students')
+    .get('http://notesapplication.brocktubre.com/api/v1/notes')
     .then((value: any) => {
+      this.loadingNotes = false;
       console.log(value);
     });
   }
