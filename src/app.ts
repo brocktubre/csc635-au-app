4fa95007cc247a88d0abd1c0124ea7ece1608289
Brock Tubre <brock.tubre@csra.com>
2018-04-23 17:57:40 -0500

    Added comments

--- src/app.ts
+++ src/app.ts
@@ -11,29 +11,29 @@ export class App {
     this.year = new Date().getFullYear();
   }
 
-  public configureRouter(config: RouterConfiguration, router: Router){
-    this.router = router;
-    config.title = 'CSC 635 - Final App';
-    config.map([
-      { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('./home') },
+  // public configureRouter(config: RouterConfiguration, router: Router){
+  //   this.router = router;
+  //   config.title = 'CSC 635 - Final App';
+  //   config.map([
+  //     { route: ['', 'home'], name: 'home', moduleId: PLATFORM.moduleName('./home') },
 
-      { route: ['notes'], name: 'notes', moduleId: PLATFORM.moduleName('./notes/list') },
-      { route: ['create-note'], name: 'create-note', moduleId: PLATFORM.moduleName('./notes/create') },
-      { route: ['edit-note/:id'], name: 'edit-note', moduleId: PLATFORM.moduleName('./notes/edit') },
+  //     { route: ['notes'], name: 'notes', moduleId: PLATFORM.moduleName('./notes/list') },
+  //     { route: ['create-note'], name: 'create-note', moduleId: PLATFORM.moduleName('./notes/create') },
+  //     { route: ['edit-note/:id'], name: 'edit-note', moduleId: PLATFORM.moduleName('./notes/edit') },
 
-      { route: ['users'], name: 'users', moduleId: PLATFORM.moduleName('./users/list') },
-      { route: ['create-user'], name: 'create-user', moduleId: PLATFORM.moduleName('./users/create') },
-      { route: ['edit-user/:id'], name: 'edit-user', moduleId: PLATFORM.moduleName('./users/edit') },
+  //     { route: ['users'], name: 'users', moduleId: PLATFORM.moduleName('./users/list') },
+  //     { route: ['create-user'], name: 'create-user', moduleId: PLATFORM.moduleName('./users/create') },
+  //     { route: ['edit-user/:id'], name: 'edit-user', moduleId: PLATFORM.moduleName('./users/edit') },
 
-      { route: ['categories'], name: 'categories', moduleId: PLATFORM.moduleName('./categories/list') },
-      { route: ['create-category'], name: 'create-category', moduleId: PLATFORM.moduleName('./categories/create') },
-      { route: ['edit-category/:id'], name: 'edit-category', moduleId: PLATFORM.moduleName('./categories/edit') },
+  //     { route: ['categories'], name: 'categories', moduleId: PLATFORM.moduleName('./categories/list') },
+  //     { route: ['create-category'], name: 'create-category', moduleId: PLATFORM.moduleName('./categories/create') },
+  //     { route: ['edit-category/:id'], name: 'edit-category', moduleId: PLATFORM.moduleName('./categories/edit') },
 
-    ]);
-    config.mapUnknownRoutes('./home');
-  }
+  //   ]);
+  //   config.mapUnknownRoutes('./home');
+  // }
 
-  public navigateHome(){
-    this.router.navigateToRoute('home');
-  }
+  // public navigateHome(){
+  //   this.router.navigateToRoute('home');
+  // }
 }
--- src/redux/markdown.ts
+++ src/redux/markdown.ts
@@ -1,21 +1,34 @@
+// https://www.sitepoint.com/managing-state-aurelia-with-redux/
+
 import marked from 'marked';
 import { bindable } from 'aurelia-framework';
 import { createStore } from 'redux';
 import undoable from 'redux-undo';
 import { ActionCreators } from 'redux-undo';
 
+// For simplcity we are keeping the 
+// reducer and action creator in the same file.
 export class Markdown {
   @bindable raw;
   html = '';
+
+  // This initilizes our store
   store = createStore(undoable(textUpdater));
+
   pastCount = 0;
   futureCount = 0;
 
   constructor() {
+    // This subscription will register an update callback
+    // that the Redux store will call any time an
+    // action has been dispatched.
     this.store.subscribe(this.update.bind(this));
   }
 
   update() {
+    // The update method itself just requests the latest state
+    // from the store using Redux’s getState method and
+    // assigns the resulting values to our html and raw properties.
     const state = this.store.getState()['present'];
     this.html = state.html;
     this.raw = state.raw;
@@ -50,7 +63,8 @@ const updateText = (text) => {
   };
 };
 
-// reducer
+// reducer function. Reducers are pure function that takes the state of the app
+// and the action being dispatched and return the next state of the app.
 function textUpdater(state = { raw: '', html: '' }, action) {
   switch (action.type) {
   case TEXT_UPDATE:
